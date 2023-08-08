19e5b68811a5583f59c0b9bfdcaf54268a45975f
Yosef Gebreyesus <withstunna005@gmail.com>
2023-08-08 18:49:22 +0300

    Add evaluating function and event listeners

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let firstOperand;
-let operation;
-let secondOperand;
-
+let snarkyString = "HAHAHAHA";
 function operate(operator, firstOperand, secondOperand){
     let result;
     switch(operator) {
@@ -11,7 +8,7 @@ function operate(operator, firstOperand, secondOperand){
         case '-':
             result = subtract(firstOperand, secondOperand);
             return result;
-        case '*':
+        case 'x':
             result = multiply(firstOperand, secondOperand);
             return result;
         case '/':
@@ -21,7 +18,7 @@ function operate(operator, firstOperand, secondOperand){
 }
 
 function add(a, b){
-    return a+b;
+    return +a + +b;
 }
 
 function subtract(a, b){
@@ -33,5 +30,91 @@ function multiply(a, b){
 }
 
 function divide(a,b){
+    if (b===0){
+        return snarkyString;
+    }
     return a/b;
 }
+
+let allOperations = ['+', '-', '/', 'x'];
+
+const allButtons = Array.from(document.querySelectorAll('button'));
+
+const display = document.querySelector('.display');
+display.textContent = 0;
+
+for (const button of allButtons){
+    button.addEventListener('click', ()=>{
+        if (display.textContent === snarkyString){
+            display.textContent = "";
+        }
+    })
+    // if clear
+    if (button.textContent === "clear"){
+        
+        button.addEventListener('click', ()=>{
+            display.textContent = 0;
+            
+        });
+    }
+    
+    // if 0-9 number
+    else if(!isNaN(button.textContent) || button.textContent === "."){
+        
+        button.addEventListener('click', ()=>{
+            if(display.textContent === "0") display.textContent = "";
+            
+            display.textContent += button.textContent;
+        });
+    }
+    else{
+        if (allOperations.includes(button.textContent)){
+            button.addEventListener('click', ()=>{
+                    if(display.textContent === "0") display.textContent = "";
+                    display.textContent = evaluate(display.textContent);
+                    display.textContent += button.textContent;
+            });
+        }
+        else if(button.textContent === "="){
+            button.addEventListener('click', ()=>{
+                if(display.textContent === "0") display.textContent = "";
+                display.textContent = evaluate(display.textContent);
+            });
+        }
+    }
+}
+
+// display.textContent = "";
+
+function evaluate(expression){
+    if (!isNaN(expression)){
+        return Math.round(expression*100)/100;
+    }
+
+    let operator;
+    if (expression.includes('+')){
+        operator = '+';
+    }
+    else if (expression.includes('-')){
+        operator = '-';
+    }
+    else if (expression.includes('x')){
+        operator = 'x';
+    }
+    else if (expression.includes('/')){
+        operator = '/';
+    }
+
+    
+    let operands = expression.split(operator);
+    let firstOperand = operands[0];
+    let secondOperand = operands[1];
+    if (operator === '/' && Number(secondOperand) === 0){
+        return 'HAHAHAHA';
+    }
+    
+    let finalResult = operate(operator, firstOperand, secondOperand);
+
+    return Math.round(finalResult*100)/100;
+}
+
